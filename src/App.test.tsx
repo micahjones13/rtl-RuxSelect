f9f791348ae83b72e78af144e439d8b18711aa1f
Micah Jones <micah.jones@rocketcom.com>
2024-08-01 13:01:55 -0600

    make chai work, select menu test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import { screen, within } from "shadow-dom-testing-library";
 
 import App from "./App";
+import { userEvent } from "@testing-library/user-event";
 
 it("should have hello world", () => {
   render(<App />);
@@ -15,3 +17,38 @@ it("should fire change event", async () => {
   fireEvent.change(selectMenu, { target: { value: "2" } });
   expect(selectMenu.value).toBe("2");
 });
+it("should user change on input", async () => {
+  const user = userEvent.setup();
+  render(<App />);
+  const ruxInput: HTMLRuxInputElement = screen.getByTestId("input");
+  const shadowInput: HTMLInputElement = await within(
+    ruxInput
+  ).findByShadowLabelText("Input Time");
+  //* Also works to find by role.
+  //   const shadowInput: HTMLInputElement = await within(ruxInput).findByShadowRole(
+  //     "textbox"
+  //   );
+
+  expect(shadowInput.value).toBe("");
+  await user.type(shadowInput, "test");
+  expect(shadowInput.value).toBe("test");
+});
+it("should use user event to change select value", async () => {
+  const user = userEvent.setup();
+  render(<App />);
+  const resultDiv = screen.getByTestId("result");
+  expect(resultDiv).toHaveTextContent("Selected option: 1");
+  const selectMenu: HTMLRuxSelectElement = await screen.findByTestId(
+    "select-menu"
+  );
+  const shadowSelect: HTMLSelectElement = await within(
+    selectMenu
+  ).findByShadowRole("combobox");
+  await user.selectOptions(shadowSelect, "2");
+  expect(resultDiv).toHaveTextContent("Selected option: 2");
+});
+// it("finds native select just fine", async () => {
+//   render(<App />);
+//   const nativeSelect = screen.getByRole("combobox");
+//   expect(nativeSelect).toBeDefined();
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import "@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css";
 
-import { RuxOption, RuxSelect } from "@astrouxds/react";
+import { RuxInput, RuxOption, RuxSelect } from "@astrouxds/react";
+
+import { useState } from "react";
 
 function App() {
+  const [selectedVal, setSelectedVal] = useState<string | string[] | undefined>(
+    "1"
+  );
   function handleChange(e: Event) {
-    console.log("change handleded: ", e);
+    const el = e.target as HTMLRuxSelectElement;
+    setSelectedVal(el!.value);
   }
 
   return (
@@ -19,11 +25,17 @@ function App() {
         onRuxchange={(e) => {
           handleChange(e);
         }}
+        label="Select"
+        name="test-name"
       >
         <RuxOption value="1" label="One"></RuxOption>
-        <RuxOption value="2" label="Two"></RuxOption>
+        <RuxOption value="2" label="Two" data-testid="option"></RuxOption>
       </RuxSelect>
+      <div data-testid="result">Selected option: {selectedVal}</div>
       <br />
+      <RuxInput data-testid="input" label="Input Time"></RuxInput>
+      <br />
+      <label>Native Select</label>
       <select>
         <option value="1">One</option>
         <option value="2">Two</option>
